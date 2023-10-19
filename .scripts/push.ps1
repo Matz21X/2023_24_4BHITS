15ca6aacf98b61dc6a0b70c6ffba24fe117807b1
maz <matthi.hrbek@outlook.com>
2023-10-19 23:59:38 +0200

    Vault backup: 19/10/23 23:59:38 MAZ-PC SHUTDOWN

--- .scripts/push.ps1
+++ .scripts/push.ps1
@@ -1,6 +1,11 @@
 Set-Location "C:\Users\matth\Documents\2023_24_4BHITS"
 $commitMessage = "Vault backup: $(Get-Date -Format 'dd/MM/yy HH:mm:ss') $env:COMPUTERNAME SHUTDOWN"
 
+Write-Host "START"
+
 git add -A
 git commit -m $commitMessage
 git push
+
+Start-Sleep -Seconds 10
+Write-Host "END"
