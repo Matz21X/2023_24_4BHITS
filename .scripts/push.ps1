a04b765e0c112d8ae24b0096c080e9c321e75e59
maz <matthi.hrbek@outlook.com>
2023-10-20 00:49:29 +0200

    Vault backup: 20/10/23 00:49:29 MAZ-PC SHUTDOWN

--- .scripts/push.ps1
+++ .scripts/push.ps1
@@ -1,6 +1,5 @@
 Set-Location "C:\Users\matth\Documents\2023_24_4BHITS"
 $commitMessage = "Vault backup: $(Get-Date -Format 'dd/MM/yy HH:mm:ss') $env:COMPUTERNAME SHUTDOWN"
-$logFilePath = "C:\Users\matth\Documents\2023_24_4BHITS\.scripts\pushLog.txt"
 
 git add -A
 git commit -m $commitMessage
