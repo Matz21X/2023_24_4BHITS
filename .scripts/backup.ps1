eaba1208fec305fedbc9a9d552abae1d9dc9669a
maz <matthi.hrbek@outlook.com>
2023-10-19 23:02:34 +0200

    Vault backup: 19/10/23 23:02:34 ASUS
    Affected files:
    .obsidian/workspace.json
    .scripts/backup.ps1
    .scripts/gitpull.ps1

--- .scripts/backup.ps1
+++ .scripts/backup.ps1
@@ -1,34 +1,52 @@
 $sourcePath = "C:\Users\matth\Documents\2023_24_4BHITS"
 $destinationPath = "C:\Users\matth\OneDrive - HTL Hollabrunn\2023_24_4BHITS"
-$logFilePath = "C:\Users\matth\Documents\2023_24_4BHITS\.scripts\copyLog.txt" # Pfad zum Logfile
-$hostname = $env:COMPUTERNAME # Hostname des Computers abrufen
+$logFilePath = "C:\Users\matth\Documents\2023_24_4BHITS\.scripts\copyLog.txt"
+$hostname = $env:COMPUTERNAME
 
-# Überprüfen, ob der Quellordner existiert
-if (Test-Path $sourcePath -PathType Container) {
-    # Überprüfen, ob der Zielordner existiert. Wenn nicht, erstellen Sie ihn.
-    if (-not (Test-Path $destinationPath -PathType Container)) {
-        New-Item -Path $destinationPath -ItemType Directory -Force
-    }
+Set-Location C:\Users\matth\Documents\2023_24_4BHITS
+$gitPullOutput = git pull
 
-    # Versuchen, Dateien von Quelle in Ziel zu kopieren und überschreiben, .git-Ordner ausschließen
-    try {
-        Copy-Item -Path "$sourcePath\*" -Destination $destinationPath -Force -Recurse -Exclude ".git"
-        Write-Host "Kopieren abgeschlossen."
-        
-        # Logbuch schreiben
-        "-------------------------------------------------" | Out-File -Append -FilePath $logFilePath
-        "Kopieren abgeschlossen: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
-        "Hostname: $hostname" | Out-File -Append -FilePath $logFilePath
-    } catch {
-        Write-Host "Fehler beim Kopieren: $_"
-        
-        # Fehlermeldung im Logbuch festhalten
-        "Fehler beim Kopieren: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
-        $_.Exception.Message | Out-File -Append -FilePath $logFilePath
-    }
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "SUCCESS"
+
+    updateOneDrive
+
+
+
 } else {
-    Write-Host "Der Quellordner existiert nicht."
+    Write-Host "ERROR: Der Git Pull-Vorgang war nicht erfolgreich."
+}
+
+Write-Host "Git Pull-Ausgabe: $gitPullOutput"
+
+
+
+
+
+function updateOneDrive {
+    Write-Host "----------ONEDRIVE UPDATE----------"
+    if (Test-Path $sourcePath -PathType Container) {
+        if (-not (Test-Path $destinationPath -PathType Container)) {
+            New-Item -Path $destinationPath -ItemType Directory -Force
+        }
     
-    # Fehlermeldung im Logbuch festhalten
-    "Der Quellordner existiert nicht: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
+        try {
+            Copy-Item -Path "$sourcePath\*" -Destination $destinationPath -Force -Recurse -Exclude ".git"
+            Write-Host "Kopieren abgeschlossen."
+            
+            "-------------------------------------------------" | Out-File -Append -FilePath $logFilePath
+            "Kopieren abgeschlossen: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
+            "Hostname: $hostname" | Out-File -Append -FilePath $logFilePath
+        } catch {
+            Write-Host "Fehler beim Kopieren: $_"
+            
+            "Fehler beim Kopieren: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
+            $_.Exception.Message | Out-File -Append -FilePath $logFilePath
+        }
+    } else {
+        Write-Host "Der Quellordner existiert nicht."
+        
+        "Der Quellordner existiert nicht: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
+    }
+
 }
--- .scripts/gitpull.ps1
+++ .scripts/gitpull.ps1
@@ -1,2 +1,11 @@
 Set-Location C:\Users\matth\Documents\2023_24_4BHITS
-git pull
+$gitPullOutput = git pull
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "SUCCESS"
+} else {
+    Write-Host "ERROR: Der Git Pull-Vorgang war nicht erfolgreich."
+}
+
+Write-Host "Git Pull-Ausgabe: $gitPullOutput"
+Pause
