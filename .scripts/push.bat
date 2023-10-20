ea99882c0fe4e1448a366b8c50985df56c33ca75
maz <matthi.hrbek@outlook.com>
2023-10-20 19:03:15 +0200

    Scripts edited

--- .scripts/push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\matth\Documents\2023_24_4BHITS"
-set commitMessage=Vault backup: %date:~0,2%/%date:~3,2%/%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2% %COMPUTERNAME% SHUTDOWN
-
-git add -A
-git commit -m "%commitMessage%"
-git push
