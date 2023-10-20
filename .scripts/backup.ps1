ca3fa1077fb128229a2a6668ead13a151ce63392
maz <matthi.hrbek@outlook.com>
2023-10-20 19:04:10 +0200

    Scripts edited

--- .scripts/backup.ps1
+++ .scripts/backup.ps1
@@ -9,26 +9,16 @@ function UpdateOneDrive {
             Copy-Item -Path "$sourcePath\*" -Destination $destinationPath -Force -Recurse -Exclude ".git"
             Write-Host "Kopieren abgeschlossen."
             
-            "-------------------------------------------------" | Out-File -Append -FilePath $logFilePath
-            "Kopieren abgeschlossen: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
-            "Hostname: $hostname" | Out-File -Append -FilePath $logFilePath
         } catch {
             Write-Host "Fehler beim Kopieren: $_"
-            
-            "Fehler beim Kopieren: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
-            $_.Exception.Message | Out-File -Append -FilePath $logFilePath
         }
     } else {
         Write-Host "Der Quellordner existiert nicht."
-        
-        "Der Quellordner existiert nicht: $(Get-Date)" | Out-File -Append -FilePath $logFilePath
     }
 }
 
 $sourcePath = "C:\Users\matth\Documents\2023_24_4BHITS"
 $destinationPath = "C:\Users\matth\OneDrive - HTL Hollabrunn\2023_24_4BHITS"
-$logFilePath = "C:\Users\matth\Documents\2023_24_4BHITS\.scripts\copyLog.txt"
-$hostname = $env:COMPUTERNAME
 
 Set-Location "C:\Users\matth\Documents\2023_24_4BHITS"
 $gitPullOutput = git pull
