7c674c4597717c0fcbf40394aef312cbf86bc22e
maz <matthi.hrbek@outlook.com>
2023-10-22 21:20:13 +0200

    Backup internet check implemented

--- .scripts/backup.ps1
+++ .scripts/backup.ps1
@@ -1,3 +1,7 @@
+$sourcePath = "C:\Users\matth\Documents\2023_24_4BHITS"
+$destinationPath = "C:\Users\matth\OneDrive - HTL Hollabrunn\2023_24_4BHITS"
+Set-Location "C:\Users\matth\Documents\2023_24_4BHITS"
+
 function UpdateOneDrive {
     Write-Host "----------ONEDRIVE UPDATE----------"
     if (Test-Path $sourcePath -PathType Container) {
@@ -17,17 +21,39 @@ function UpdateOneDrive {
     }
 }
 
-$sourcePath = "C:\Users\matth\Documents\2023_24_4BHITS"
-$destinationPath = "C:\Users\matth\OneDrive - HTL Hollabrunn\2023_24_4BHITS"
+function Test-InternetConnection {
+    $ErrorActionPreference = "Stop"
+    try {
+        $pingResult = Test-Connection -ComputerName "www.google.com" -Count 1 -ErrorAction SilentlyContinue
+        if ($null -ne $pingResult) {
+            Write-Host "Internet is reachable."
+            return $true
+        } else {
+            Write-Host "Internet is not reachable."
+            return $false
+        }
+    } catch {
+        Write-Host "Internet is not reachable."
+        return $false
+    }
+}
 
-Set-Location "C:\Users\matth\Documents\2023_24_4BHITS"
-$gitPullOutput = git pull
-
-if ($LASTEXITCODE -eq 0) {
+function GitPull {
+    $gitPullOutput = git pull
+    if ($LASTEXITCODE -eq 0) {
     Write-Host "Git Pull-Ausgabe: $gitPullOutput"
     UpdateOneDrive
 } else {
     Write-Host "ERROR: Der Git Pull-Vorgang war nicht erfolgreich."
     Write-Host "Git Pull-Ausgabe: $gitPullOutput"
+    Pause
+}
+
+}
+if (Test-InternetConnection) {
+    Write-Host "Bruh"
+    GitPull
+} else {
+    Write-Host "Cannot perform tasks that require an internet connection."
     Pause
 }
