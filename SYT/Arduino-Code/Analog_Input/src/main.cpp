e6b02920a6bc14bcac1131b58322102355205fc5
maz <127680394+Matz21X@users.noreply.github.com>
2023-11-22 08:57:24 +0100

    Vault backup: 22/11/23 08:57:24 XMG
    Affected files:
    SYT/Arduino-Code/Analog_Input/src/main.cpp

--- SYT/Arduino-Code/Analog_Input/src/main.cpp
+++ SYT/Arduino-Code/Analog_Input/src/main.cpp
@@ -6,10 +6,17 @@
 void setup() {
   Serial.begin(9600);
   pinMode(36, ANALOG );
+  ledcSetup(0, 144, 8);
+  ledcAttachPin( 18, 0 );
+}
+
+int getValue(int analogValue) {
+  return analogValue / 16;
 }
 
 void loop() {
   Serial.println( analogRead(36));
-  delay( 1000 );
-}
 
+    ledcWrite(0, getValue( analogRead(36))); 
+  
+}
