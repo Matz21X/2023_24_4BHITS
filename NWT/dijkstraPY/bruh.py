498c83f5561ca9f804762f9066ca98ce595a1cdd
maz <127680394+Matz21X@users.noreply.github.com>
2023-11-22 15:24:30 +0100

    Vault backup: 22/11/23 15:24:30 XMG
    Affected files:
    .obsidian/workspace.json
    NWT/dijkstraPY/bruh.py

--- NWT/dijkstraPY/bruh.py
+++ NWT/dijkstraPY/bruh.py
@@ -1,9 +1,11 @@
-list1 = ["a", "b", "c", "d"]
-list2 = [25.50, True, -55, 1+2j]
-list4 = ["Rohan", "Physics", 21, 69.75]
-list3 = [1, 2, 3, 4, 5]
+ls =[]
+inp = open("input.txt", "r")
 
-print ("Items from index 1 to last in list1: ", list1[1:])
-print ("Items from index 0 to 1 in list2: ", list2[:2])
-print ("Items from index 2 to last in list3", list3[2:-1])
-print ("Items from index 0 to index last in list4", list4[:])
+for line in inp:
+    print(line)
+    line1 = line
+    line1.(0)
+    line1.pop(0)
+    ls.append(line)
+
+    print(ls[2])
