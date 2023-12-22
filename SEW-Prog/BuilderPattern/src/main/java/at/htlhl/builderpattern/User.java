8a5e48bfcbc34ac4af4fd4bae1835d6459ef985d
maz <127680394+Matz21X@users.noreply.github.com>
2023-12-22 10:26:50 +0100

    Vault backup: 22/12/23 10:26:50 XMG
    Affected files:
    SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/User.java

--- SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/User.java
+++ SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/User.java
@@ -2,12 +2,52 @@ package at.htlhl.builderpattern;
 
 public class User {
     // All final attributes
-    private final String firstName;
-    private final String lastName;
-    private final int age;
-    private final String phone;
-    private final String address;
+    private final String firstName;     // Required
+    private final String lastName;      // Required
+    private final int age;              // Optional
+    private final String phone;         // optional
+    private final String address;       // optional
 
-    private User
+    private User(UserBuilder builder) {
 
+    }
+
+    // All getter and NO setter
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", age=" + age +
+               ", phone='" + phone + '\'' +
+               ", address='" + address + '\'' +
+               '}';
+    }
+
+    public static class UserBuilder{
+        private final String firstName;
+        private final String lastName;
+        private int age;    
+    }
 }
