fc7c810a0e6ada817d17abf2b9d9ad9a2d5c698f
maz <127680394+Matz21X@users.noreply.github.com>
2023-12-22 10:15:50 +0100

    Vault backup: 22/12/23 10:15:50 XMG
    Affected files:
    .obsidian/workspace.json
    SEW-Prog/BuilderPattern/.gitignore
    SEW-Prog/BuilderPattern/.idea/.gitignore
    SEW-Prog/BuilderPattern/.idea/encodings.xml
    SEW-Prog/BuilderPattern/.idea/misc.xml
    SEW-Prog/BuilderPattern/.idea/uiDesigner.xml
    SEW-Prog/BuilderPattern/.idea/vcs.xml
    SEW-Prog/BuilderPattern/pom.xml
    SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/User.java

---
+++ SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/User.java
@@ -0,0 +1,13 @@
+package at.htlhl.builderpattern;
+
+public class User {
+    // All final attributes
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final String phone;
+    private final String address;
+
+    private User
+
+}
