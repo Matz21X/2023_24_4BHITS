ab9b378a7f1e705df6c7a4de8f129685e3314304
maz <matthi.hrbek@outlook.com>
2024-01-08 16:22:39 +0100

    Vault backup: 08/01/24 16:22:39 ASUS
    Affected files:
    .obsidian/workspace.json
    INSY/Projekt/REST/test.js

--- INSY/Projekt/REST/test.js
+++ INSY/Projekt/REST/test.js
@@ -1,4 +1,11 @@
-const readFile = require('node:fs')
-const bruh = readFile('./oasch.txt', 'utf-8',);
+const fs = require('node:fs');
+
+const bruh = fs.readFile('./oasch.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+});
 
 console.log(bruh)
