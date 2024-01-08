7bf8ef7bbdbe07220f4c997764836f2e5638ea28
maz <matthi.hrbek@outlook.com>
2024-01-08 18:35:40 +0100

    Vault backup: 08/01/24 18:35:39 ASUS
    Affected files:
    INSY/Projekt/REST/oasch.txt
    INSY/Projekt/REST/package.json
    INSY/Projekt/REST/test.js

--- INSY/Projekt/REST/test.js
+++
@@ -1,6 +0,0 @@
-// Bei sync wartet der Code bis das File geladen ist (ungut 😬)
-import {readFileSync} from 'node:fs';  // Importiert Filesystem Modul
-
-const fileContent = readFileSync('./oasch.txt', 'utf8');
-
-console.log(fileContent);  
