2f870439339aab557d2102d224fd2a4eaacafbd1
maz <127680394+Matz21X@users.noreply.github.com>
2024-01-10 09:06:54 +0100

    Vault backup: 10/01/24 09:06:54 XMG
    Affected files:
    SYT/Arduino-Code/TempServer/platformio.ini
    SYT/Arduino-Code/TempServer/src/main.cpp

--- SYT/Arduino-Code/TempServer/src/main.cpp
+++ SYT/Arduino-Code/TempServer/src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+OneWire ow( 4 );
+DallasTemperature ts( &ow );
+const char* ssid = "HTLIoT";
+const char* password = "hollabrunn";
+
+WebServer server(80);
+
+void handleRoot() {
+  float tempBoard1 = getTemperatureFromBoard1();
+  float tempBoard2 = getTemperatureFromBoard2();
+
+  String tempData = "Temperatur von Board 1: " + String(tempBoard1) + "°C\n";
+  tempData += "Temperatur von Board 2: " + String(tempBoard2) + "°C";
+
+  server.send(200, "text/plain", tempData);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Verbindung zum WiFi wird hergestellt...");
+  }
+  Serial.println("Verbunden mit dem WiFi");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, handleRoot);
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
+  // Ihr Code hier
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float getTemperatureFromBoard1() {
+  // Code für die Temperaturmessung von Board 1 hier einfügen
+  ts.requestTemperatures( );
+  float currentTemp = ts.getTempCByIndex(0);  
+  return currentTemp;
+}
+
+float getTemperatureFromBoard2() {
+  float temperature = requestTemperatureFromBoard2();
+  return temperature;
+}
+
+float requestTemperatureFromBoard2() {
+  WiFiClient client;
+  if (client.connect("IP-des-zweiten-ESP32", 80)) {
+    client.print("GET / HTTP/1.1\r\nHost: IP-des-zweiten-ESP32\r\nConnection: close\r\n\r\n");
+    delay(500); // Warten auf die Antwort
+    String payload = client.readStringUntil('\r');
+    client.stop();
+    return payload.toFloat();
+  } else {
+    Serial.println("Fehler beim Verbinden mit Board 2");
+    return -1.0; // Fehlerwert
+  }
 }
