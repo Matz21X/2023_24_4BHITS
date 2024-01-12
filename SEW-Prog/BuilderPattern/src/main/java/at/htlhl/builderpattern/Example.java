71342f70999e4ab8a0d477b6b2d2b6f13a5e9e65
maz <127680394+Matz21X@users.noreply.github.com>
2024-01-12 10:21:28 +0100

    Vault backup: 12/01/24 10:21:28 XMG
    Affected files:
    .obsidian/workspace.json
    SEW-Prog/BuilderPattern/.idea/misc.xml
    SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/Example.java

--- SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/Example.java
+++ SEW-Prog/BuilderPattern/src/main/java/at/htlhl/builderpattern/Example.java
@@ -1,5 +1,26 @@
 package at.htlhl.builderpattern;
 
 public class Example {
-    public
+    public static void main (String [] args){
+        User user1 = new User.UserBuilder("Bart", "Simpson")
+                .age(15)
+                .phone("1234567")
+                .address("Springfield")
+                .build();
+        System.out.println(user1);
+
+        User user2 = new User.UserBuilder("James", "Bond")
+                .age(52)
+                .phone("007")
+                // no address
+                .build();
+        System.out.println(user2);
+
+        User user3 = new User.UserBuilder("Super", "Man")
+                // no age
+                // no phone
+                // no address
+                .build();
+        System.out.println(user3);
+    }
 }
