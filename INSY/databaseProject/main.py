f499d05168c167a2d7046b1d7ae9506bc66612e4
maz <matthi.hrbek@outlook.com>
2024-01-15 16:42:40 +0100

    Vault backup: 15/01/24 16:42:40 ASUS
    Affected files:
    .obsidian/workspace.json
    INSY/databaseProject/main.py
    INSY/datenbankDesign.drawio

--- INSY/databaseProject/main.py
+++ INSY/databaseProject/main.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 
 # API-URLs für den Datenbankzugriff
 api_urls = {
-    'produkte': 'http://localhost:3000/produkte',
+    'Users': 'http://localhost:3000/produkte',
     'bestellungen': 'http://localhost:3000/bestellungen',
     'lager': 'http://localhost:3000/lager',
     'kunden': 'http://localhost:3000/kunden'  # Neuer Tab für die Tabelle "kunden"
