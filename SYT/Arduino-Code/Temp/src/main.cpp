ef2a9508dca20648605f704a7a3daa0c6471579f
maz <127680394+Matz21X@users.noreply.github.com>
2024-01-31 08:36:32 +0100

    Vault backup: 31/01/24 08:36:31 XMG
    Affected files:
    .obsidian/workspace.json
    Backlog_0124.md
    SYT/Arduino-Code/Temp/src/main.cpp

--- SYT/Arduino-Code/Temp/src/main.cpp
+++ SYT/Arduino-Code/Temp/src/main.cpp
@@ -3,31 +3,59 @@
 #include <OneWire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wifi.h>
+#include <WebServer.h>
 
-OneWire ow( 4 );
-DallasTemperature ts( &ow );
-Adafruit_SSD1306 dp( 124, 64, &Wire, -1 );
+const char *ssid = "HTLIot";
+const char *password = "hollabrunn";
 
-void setup() {
+OneWire ow(4);
+DallasTemperature ts(&ow);
+Adafruit_SSD1306 dp(124, 64, &Wire, -1);
+WebServer server( 80 );
+
+void setup()
+{
   Serial.begin(9600);
-  dp.begin( SSD1306_SWITCHCAPVCC, 0x3C );
+  dp.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   dp.clearDisplay();
   dp.setTextColor(SSD1306_WHITE);
-  dp.setTextSize(2);  // Ändere die Textgröße nach Bedarf
+  dp.setTextSize(2);
   dp.setCursor(0, 0);
   dp.display();
-  ts.begin( );
+  ts.begin();
+  setupWiFi();
+  server.on("/", HTTP_GET, handleRoot);
 }
 
-void loop() {
-  ts.requestTemperatures( );
-  float currentTemp = ts.getTempCByIndex(0);  
+void loop()
+{
+  server.handleClient();
+  ts.requestTemperatures();
+  float currentTemp = ts.getTempCByIndex(0);
   Serial.print(currentTemp);
   Serial.println("ºC");
   dp.clearDisplay();
   dp.setCursor(20, 8);
   dp.println(currentTemp);
   dp.display();
-  // delay(500);
+  delay(1000); // Füge eine kurze Verzögerung hinzu, um die Aktualisierungsrate zu steuern
 }
 
+void setupWiFi()
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi: " + WiFi.localIP());
+}
+
+void handleRoot()
+{
+  String tempString = String(ts.getTempCByIndex(0));
+  String html = "<html><body><h1>Temperatur: " + tempString + " &deg;C</h1></body></html>";
+  server.send(200, "text/html", html);
+}
