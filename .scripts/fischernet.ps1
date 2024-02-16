1f5b7860b83108f66ba0e7e3b8833b0c347a4ff1
maz <127680394+Matz21X@users.noreply.github.com>
2024-02-16 13:00:00 +0100

    Vault backup: 16/02/24 12:59:59 XMG
    Affected files:
    .scripts/fischernet.ps1

--- .scripts/fischernet.ps1
+++ .scripts/fischernet.ps1
@@ -2,15 +2,15 @@ $logfile = "C:\Users\matth\Documents\2023_24_4BHITS\FischerNet.md"
 $destination = "8.8.8.8"
 
 while($true) {
+    $currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $pingResult = Test-Connection -ComputerName $destination -Count 1 -Quiet
     if (-not $pingResult) {
-        $currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
         Add-Content -Path $logfile -Value "- Ping fehlgeschlagen um $currentTime"
     }
     if ($pingResult) {
-	    echo "Hell yea B)"
+	    echo "Hell yea B) $currentTime"
     } else {
-	    echo "oasch"
+	    echo "Oasch $currentTime"
     }
 	Start-Sleep -Seconds 5
 }
