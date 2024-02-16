58ac87992930855e130ffddc47c59cbc2758a9b4
maz <127680394+Matz21X@users.noreply.github.com>
2024-02-16 08:36:59 +0100

    Vault backup: 16/02/24 08:36:59 XMG
    Affected files:
    .obsidian/workspace.json
    .scripts/fischernet.ps1
    FischerNet.md

--- .scripts/fischernet.ps1
+++ .scripts/fischernet.ps1
@@ -1,12 +1,16 @@
-$logfile = "C:\Users\matth\Documents\2023_24_4BHITS\.scripts\net.log"
+$logfile = "C:\Users\matth\Documents\2023_24_4BHITS\FischerNet.md"
 $destination = "8.8.8.8"
 
 while($true) {
     $pingResult = Test-Connection -ComputerName $destination -Count 1 -Quiet
     if (-not $pingResult) {
         $currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Add-Content -Path $logfile -Value "Ping fehlgeschlagen um $currentTime"
+        Add-Content -Path $logfile -Value "- Ping fehlgeschlagen um $currentTime"
     }
-    echo $pingResult
+    if ($pingResult) {
+	    echo "Hell yea B)"
+    } else {
+	    echo "oasch"
+    }
 	Start-Sleep -Seconds 5
 }
--- FischerNet.md
+++ FischerNet.md
@@ -1,7 +1,3 @@
 # Internet oasch
 #FIMI 
 
-
-16.2
-- 8.05
-- 8.11
