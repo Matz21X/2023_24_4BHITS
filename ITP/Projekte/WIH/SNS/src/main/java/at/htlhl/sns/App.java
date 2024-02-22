16a02f7fafa73e037eed9cda002b12f63f4aba15
maz <127680394+Matz21X@users.noreply.github.com>
2024-02-22 15:12:26 +0100

    Vault backup: 22/02/24 15:12:25 XMG
    Affected files:
    ITP/Projekte/WIH/SNS/src/main/java/at/htlhl/sns/App.java

--- ITP/Projekte/WIH/SNS/src/main/java/at/htlhl/sns/App.java
+++ ITP/Projekte/WIH/SNS/src/main/java/at/htlhl/sns/App.java
@@ -1,28 +1,62 @@
 package at.htlhl.sns;
 
-import org.json.JSONObject;
-import org.thethingsnetwork.data.common.Connection;
-import org.thethingsnetwork.data.mqtt.Client;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 
-import java.net.URISyntaxException;
+public class Access implements MqttCallback {
+    MqttClient client;
 
-public class App {
+    public Access() {
+    }
 
-    String region = "eu";
-    String appId = "hello-world";
-    String accessKey = "2Z+MU0T5xZCaqsD0bPqOhzA6iygGFoi4FAgMFgBfXSo=";
+    public static void main(String[] args) {
+        new Access().doDemo();
+    }
 
-    Client client;
-
-    {
+    public void doDemo() {
         try {
-            client = new Client(region, appId, accessKey);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+            String username = "matthias.hrbek@it.htlhl.at";
+            String password = "Donpollo69";
+            String serverurl = "tcp://eu1.cloud.thethings.network:1883";
+            String clientId = MqttClient.generateClientId();
+
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+
+            client = new MqttClient(serverurl, clientId, null);
+            client.connect(options);
+
+            client.setCallback(this);
+            client.subscribe("#");
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
     }
 
+    @Override
 
-    client.start();
+    public void connectionLost(Throwable cause) {
+        //TODO Auto -generated method stub
 
+    }
+
+    @Override
+
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println(message);
+    }
+
+    @Override
+
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+        // TODO Auto -generated method stub
+
+    }
 }
