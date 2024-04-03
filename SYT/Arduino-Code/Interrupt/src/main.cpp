353c932bce8ce7ec17c868556a5e2d48d5dfc5a5
maz <127680394+Matz21X@users.noreply.github.com>
2024-04-03 08:24:57 +0100

    Vault backup: 03/04/24 08:24:57 XMG
    Affected files:
    SYT/Arduino-Code/Interrupt/src/main.cpp

--- SYT/Arduino-Code/Interrupt/src/main.cpp
+++ SYT/Arduino-Code/Interrupt/src/main.cpp
@@ -1,31 +1,27 @@
 #include <Arduino.h>
-
-// Pin, an dem der Schalter angeschlossen ist
-const int switchPin = 2;
-
-// Variable zur Zählung
-volatile int count = 0;
-
-void IRAM_ATTR handleInterrupt() {
-  // Interrupt-Routine: Schalter wurde betätigt
-  count++;
-}
-
-
+#define debounceZeit 50
+unsigned long alteZeit=millis();
+int counter=0;
+int oldCounter=-1;
+void IRAM_ATTR ISR();
+ 
 void setup() {
   Serial.begin(9600);
-  
-  // Initialisierung des Schalterpins
-  pinMode(switchPin, INPUT_PULLUP);
-  
-  // Attach Interrupt für den Schalter
-  attachInterrupt(digitalPinToInterrupt(switchPin), handleInterrupt, FALLING);
+  pinMode(18,INPUT_PULLDOWN);
+  attachInterrupt(18,ISR,FALLING);
+  Serial.print("begin: ");
+  Serial.print(counter);
 }
-
+void IRAM_ATTR ISR(){
+  if ((millis()-alteZeit)>debounceZeit){
+    counter++;
+    alteZeit=millis();
+  }
+}
+ 
 void loop() {
-  // Ausgabe der aktuellen Zählung auf der Konsole
-  Serial.println("Count: " + String(count));
-  
-  // Kurze Verzögerung, um die Serielle Konsole nicht zu überlasten
-  delay(500);
+  if(oldCounter!=counter){
+    Serial.println(counter);
+    oldCounter=counter;
+  }
 }
